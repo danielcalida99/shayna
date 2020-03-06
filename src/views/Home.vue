75f112ee1a8d91766c9dacfbec1c2f5633864345
Daniel Calida <danielcalida@Daniels-MacBook-Pro.local>
2020-03-06 14:28:54 +0800

    upload project vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderShayna/>
+    <HeroShayna/>
+    <WomanShayna/>
+    <InstaShayna/>
+    <LogoShayna/>
+    <FooterShayna/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomanShayna from '@/components/WomanShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import LogoShayna from '@/components/LogoShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna,
+    InstaShayna,
+    LogoShayna,
+    FooterShayna
   }
 }
 </script>
